87d99d66749be47c0c7762d756441643dce979d2
user_4 <user_4@DESKTOP-PBCCV2O>
2023-02-09 11:11:29 +0900

    dsds

--- test/src/test/tset.java
+++ test/src/test/tset.java
@@ -1,8 +1,7 @@
 package test;
 
 public class tset {
-public static void main(String[] args) {
-	System.out.println("dd");
-	System.out.println("dlfdf");
+	public static void main(String[] args) {
+			System.out.println("323");
 }
 }
